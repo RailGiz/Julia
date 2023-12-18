f37b854ae195be2da2f0c4d67ce2a32b9e8961cb
Kemgeoljme <forsomebudytest@mail.ru>
2023-12-18 19:54:12 +0300

    Взаимодействие с помощью мыши.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,25 +1,55 @@
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.drawscope.DrawScope
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
-
+//Двойной клик - увеличение
+//Один клик передвигает
+//Долгий клик - уменьшение
 @Composable
 fun JuliaSet() {
-    Canvas(modifier = Modifier.fillMaxSize()) {
-        drawJuliaSet()
+    var scale by remember { mutableStateOf(0.9f) }
+    var offsetX by remember { mutableStateOf(0f) }
+    var offsetY by remember { mutableStateOf(0f) }
+
+    Canvas(modifier = Modifier
+        .fillMaxSize()
+        .pointerInput(Unit) {
+            detectTapGestures(
+                onPress = { position ->
+                    offsetX = position.x
+                    offsetY = position.y
+                    true
+                },
+                onLongPress = { position ->
+                    offsetX = position.x
+                    offsetY = position.y
+                    scale /= 1.1f
+                    println("Scale is now $scale")
+                },
+                onDoubleTap = { position ->
+                    offsetX = position.x
+                    offsetY = position.y
+                    scale *= 1.1f
+                    println("Scale is now $scale")
+                }
+            )
+        }) {
+        drawJuliaSet(scale, offsetX, offsetY)
     }
 }
 
-fun DrawScope.drawJuliaSet() {
+fun DrawScope.drawJuliaSet(scale: Float, offsetX: Float, offsetY: Float) {
     val width = size.width.toInt()
     val height = size.height.toInt()
-    val scale = 0.5f
-    val movex = 0f
-    val moveY = 0f
+    val movex = offsetX / width - 0.5f
+    val moveY = offsetY / height - 0.5f
     val maxiterations = 300
     val cX = -0.7f
     val cY = 0.27015f
@@ -38,7 +68,7 @@ fun DrawScope.drawJuliaSet() {
             val ratio = i.toFloat() / maxiterations
             drawRect(
                 color = Color(ratio, ratio, ratio, 1f),
-                topLeft = androidx.compose.ui.geometry.Offset(x.toFloat(), y.toFloat()),
+                topLeft = Offset(x.toFloat(), y.toFloat()),
                 size = androidx.compose.ui.geometry.Size(1f, 1f)
             )
         }
